31e2648eddac2bf3ae3d1a6704862dcd54a4940c
Ayuba Tahiru <58880393+Tayuba@users.noreply.github.com>
2021-03-28 16:08:30 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+def prime_checker(number): 
+  is_prime = True
+  for check in range(2,number):
+    if number % check == 0:
+      is_prime = False
+      
+  if is_prime == True and number > 1:
+    print("It is a prime number!.")
+  else:
+    print("It is not a prime number!.")
+
+n = int(input("Check this number: "))
+prime_checker(number=n)
+
+
+
